48bb85a2a230a4c0dc729173843858915f4f02d0
efiera <tur99267@temple.edu>
2025-02-19 22:47:48 -0500

    js components

--- src/main/webapp/_JS_CGF/MakeBook_CGF.js
+++ src/main/webapp/_JS_CGF/MakeBook_CGF.js
@@ -2,7 +2,13 @@ function MakeBook_CGF() {
 
     var ele = document.createElement("div");
 
-    var myBook = MakeBook({theTitle: "awesome", thePrice: 20, imgURL : 'pics/book_blue.jpg'});
+    var myBook = MakeBook({theTitle: "awesome", thePrice: 20, imgURL : 'pics/book_blue.jpg',
+        imgObjList: [
+            { "display": "blue book", "val": "pics/book_blue.jpg" },
+            { "display": "purple book", "val": "pics/book_purple.png" },
+            { "display": "red book", "val": "pics/book_red.png" }
+            ]
+    });
     ele.appendChild(myBook);
 
     var yourBook = MakeBook({});
--- src/main/webapp/_JS_reusable/MakeBook.js
+++ src/main/webapp/_JS_reusable/MakeBook.js
@@ -1,8 +1,9 @@
 "use strict";
 
 function MakeBook({
-    theTitle = "worker", 
+    theTitle = "default", 
     thePrice = 0,
+    imgObjList = [{"display":"Image Values Not Supplied", "val":1}],
     imgURL = null}) {
 
     var bookObj = document.createElement("div");
@@ -24,33 +25,57 @@ function MakeBook({
     };
 
     bookObj.innerHTML = `
+    ${imgURL ? `<img src='${imgURL}' class="bookImg"/>` : `<p>No Image Available</p>`}
       <div class='bookInfoClass'></div>
       <button class='titleButtonClass'>Change Title to: </button>
       <input class='newTitleInputClass'/> <br/>
       <button class='priceButtonClass'>Change Price By Amount: </button>
-      <input class='priceAddInputClass'/> 
+      <input class='priceAddInputClass'/> <br/>
+      <select class="selectImgClass">
+      </select>
     `;
-    if (imgURL == null) {
-        bookObj.innerHTML += "<p>image not available</p>";
-    } else {
-        bookObj.innerHTML += `<p><img src='${imgURL}'/></p>`;
-    };
 
+    var selectImg = bookObj.getElementsByClassName("selectImgClass")[0];
     var bookInfo = bookObj.getElementsByClassName("bookInfoClass")[0];
     var titleButton = bookObj.getElementsByClassName("titleButtonClass")[0];
     var newTitleInput = bookObj.getElementsByClassName("newTitleInputClass")[0];
     var priceButton = bookObj.getElementsByClassName("priceButtonClass")[0];
     var priceAdd = bookObj.getElementsByClassName("priceAddInputClass")[0];
+    var bookImg = bookObj.getElementsByClassName("bookImg")[0];
 
-    var display = function ( ) {
+    for (var listEle of imgObjList) {
+        var opt = document.createElement("option");
+        opt.innerHTML = listEle.display;
+        opt.value=listEle.val;
+        selectImg.appendChild(opt);
+    }
+    selectImg.value = imgURL;
+
+    selectImg.onchange = function () {
+        console.log("displaying new image");
+        //display();
+        
+        if (imgURL == null) {
+            console.log("No image available");
+        } else {
+            bookImg.src = this.value;
+        };
+    };
+
+    function display ( ) {
         bookInfo.innerHTML = `
           <p>
              Title: ${bookObj.title} <br/>
              Price: ${formatCurrency(price)} <br/>
           </p>
         `;
+        
+        console.log("display function called");
     };
     display();
+
+
+    
 
     titleButton.onclick = function () {
         bookObj.setTitle(newTitleInput.value);
